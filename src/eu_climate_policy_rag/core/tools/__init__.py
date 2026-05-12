55dc45a5d0599853f53caa17c7a4ca7efd27903d
CarSomma <carsomma@gmail.com>
2026-05-12 11:06:56 +0200

    feat: Add ToolContext, ToolExecutor, and ToolResult to tool framework exports

--- src/eu_climate_policy_rag/core/tools/__init__.py
+++ src/eu_climate_policy_rag/core/tools/__init__.py
@@ -1,6 +1,8 @@
 """Provider-neutral tool framework primitives."""
 
 from eu_climate_policy_rag.core.tools.builtin import BuiltinTool
+from eu_climate_policy_rag.core.tools.context import ToolContext
+from eu_climate_policy_rag.core.tools.executor import ToolExecutor
 from eu_climate_policy_rag.core.tools.function import FunctionTool
 from eu_climate_policy_rag.core.tools.providers import (
     PydanticSchemaProvider,
@@ -8,6 +10,7 @@ from eu_climate_policy_rag.core.tools.providers import (
     SchemaProvider,
 )
 from eu_climate_policy_rag.core.tools.registry import ToolRegistry
+from eu_climate_policy_rag.core.tools.result import ToolResult
 
 __all__ = [
     "BuiltinTool",
@@ -15,5 +18,8 @@ __all__ = [
     "PydanticSchemaProvider",
     "RawJsonSchemaProvider",
     "SchemaProvider",
+    "ToolContext",
+    "ToolExecutor",
     "ToolRegistry",
+    "ToolResult",
 ]
